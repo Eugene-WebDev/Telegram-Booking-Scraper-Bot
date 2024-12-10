06d5462fbe3e87beb5509961cd1e70307be8806c
slayd <vor4yn92@gmail.com>
2024-12-10 01:16:26 +0100

    Update files

--- bot_book_scrape.py
+++ bot_book_scrape.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     # Start the webhook for the bot
     application.run_webhook(
         listen="0.0.0.0",
-        port=8080,
+        port=8081,
         url_path=TELEGRAM_BOT_TOKEN,  # Use your bot token as the URL path
         webhook_url=f"https://scrape-project-qkwj.onrender.com/{TELEGRAM_BOT_TOKEN}",
     )
